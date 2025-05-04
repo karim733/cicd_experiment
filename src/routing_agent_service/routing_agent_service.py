19aabbe74306c5bbdb67617da3a377053a04bf14
karim733 <149538688+karim733@users.noreply.github.com>
2025-05-03 22:01:29 -0500

    Update routing_agent_service.py

--- src/routing_agent_service/routing_agent_service.py
+++ src/routing_agent_service/routing_agent_service.py
@@ -19,7 +19,8 @@ import uvicorn
 import argparse
 
 from dotenv import load_dotenv
-_ = load_dotenv('../secrets/.env')
+#_ = load_dotenv('../secrets/.env')
+os.environ('GOOGLE_API_KEY')="AIzaSyBkwtS_qq_h0QgCBOzGtsp4JbFQXI9mgDE"
 
 global_config = {}
 
